f649c0c8bf7a883b47df0528f34589b8c4e43348
Hariomk00 <justrise905@gmail.com>
2025-02-12 13:19:01 +0530

    Updated HTML & CSS for Swiper

--- script.js
+++ script.js
@@ -77,3 +77,25 @@ setInterval(fetchLiveCricketScore, 60000);
         window.open(apiURL, "_blank");
     }
 
+    
+    var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      autoplay: {
+        delay: 2000, // Adjust speed (2000ms = 2s)
+        disableOnInteraction: false,
+        reverseDirection: false // Moves from right to left
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
